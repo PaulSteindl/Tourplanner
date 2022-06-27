204d43936d492af44bddf8b617a41348cd47c856
iiLuKa <83024158+iiLuKa@users.noreply.github.com>
2022-06-27 20:23:17 +0200

    Commit frontend implementation

---
+++ Tourplanner.Models/RouteSteps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tourplanner.Models
+{
+    public class RouteSteps
+    {
+        public double Distance { get; init; }
+
+        public string? ImagePath { get; init; }
+    }
+}
