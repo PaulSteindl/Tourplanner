c189bbf43c1d21f9bf0c27f1842f740bb5256985
PaulSteindl <paul.steindl@live.com>
2022-06-28 19:10:28 +0200

    Tour Backend und DAO verschoben

---
+++ Tourplanner.DataAccessLayer/Log/ILogDAO.cs
@@ -0,0 +1,33 @@
+﻿using Tourplanner.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Tourplanner.DataAccessLayer
+{
+    public interface ILogDAO
+    {
+        /// <summary>
+        /// Fügt einen neuen Log hinzu
+        /// </summary>
+        /// <returns>True wenn eine Zeile inserted wurde</returns>
+        bool InsertLog(Log newLog);
+
+        /// <summary>
+        /// Holt alle Logs von einer Tour
+        /// </summary>
+        List<Log> SelectLogsByTourId(Guid id);
+
+        /// <summary>
+        /// Updated einen Log über die Id
+        /// </summary>
+        /// <returns>True wenn eine Zeile geupdated wurde</returns>
+        bool UpdateLogById(Log updatedLog);
+
+        /// <summary>
+        /// Löscht einen Log über die Id
+        /// </summary>
+        /// <returns>True wenn eine Zeile gelöscht wurde</returns>
+        bool DeleteLogById(Guid id);
+
+    }
+}
